993fc6626f694f16831e613e0eb97547832c24aa
unknown <hariom.fauzdar@ens.enterprises>
2026-01-06 13:30:46 +0530

    registaer and login api

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -3,10 +3,11 @@ import {
   createCategory,
   getAllCategories,
 } from "../controllers/category.controller.js";
+import { protect } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/create", createCategory);
-router.get("/getAll", getAllCategories);
+router.post("/create", protect, createCategory);
+router.get("/getAll", protect, getAllCategories);
 
 export default router;
